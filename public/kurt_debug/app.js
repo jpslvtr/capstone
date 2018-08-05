adc25c844cb6b0197575556b824239a38fd32ba6
Wezzy <kurt.m.zepeda@gmail.com>
2018-08-05 17:19:36 -0400

    Zepeda testing

---
+++ public/kurt_debug/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCFJM3V57Dci3_lsVXh0zs8nz9FoOCMB3A",
+    authDomain: "tchalla-id.firebaseapp.com",
+    databaseURL: "https://tchalla-id.firebaseio.com",
+    projectId: "tchalla-id",
+    storageBucket: "tchalla-id.appspot.com",
+    messagingSenderId: "392551642195"
+  };
+  firebase.initializeApp(config);
